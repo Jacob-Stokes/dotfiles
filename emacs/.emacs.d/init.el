452f3162fbb42f92e36f9fe93140747ca19c736c
Jacob Stokes <jacob@jacobstokes.com>
2020-07-13 23:10:40 +0100

    loads added

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -13,16 +13,46 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#242730" "#ff665c" "#7bc275" "#FCCE7B" "#51afef" "#C57BDB" "#5cEfFF" "#bbc2cf"])
  '(custom-safe-themes
    (quote
-    ("79278310dd6cacf2d2f491063c4ab8b129fee2a498e4c25912ddaa6c3c5b621e" "fe94e2e42ccaa9714dd0f83a5aa1efeef819e22c5774115a9984293af609fce7" "730a87ed3dc2bf318f3ea3626ce21fb054cd3a1471dcd59c81a4071df02cb601" "2cdc13ef8c76a22daa0f46370011f54e79bae00d5736340a5ddfe656a767fddf" "dcdd1471fde79899ae47152d090e3551b889edf4b46f00df36d653adc2bf550d" "d71aabbbd692b54b6263bfe016607f93553ea214bc1435d17de98894a5c3a086" "912cac216b96560654f4f15a3a4d8ba47d9c604cbc3b04801e465fb67a0234f0" "bf387180109d222aee6bb089db48ed38403a1e330c9ec69fe1f52460a8936b66" "9efb2d10bfb38fe7cd4586afb3e644d082cbcdb7435f3d1e8dd9413cbe5e61fc" "76bfa9318742342233d8b0b42e824130b3a50dcc732866ff8e47366aed69de11" "2f1518e906a8b60fac943d02ad415f1d8b3933a5a7f75e307e6e9a26ef5bf570" default)))
- '(org-agenda-files
-   (quote
-    ("/home/jacob/work/1/test1.org" "/home/jacob/work/1/test2.org" "/home/jacob/work/2/test2.org" "/home/jacob/work/notes/service-desk.org" "/home/jacob/work/notes/writing.org" "/home/jacob/work/programming/python/notes/python.org" "/home/jacob/work/programming/python/test2/python.org" "/home/jacob/work/programming/python/test2/test.org")))
+    ("7a994c16aa550678846e82edc8c9d6a7d39cc6564baaaacc305a3fdc0bd8725f" "99ea831ca79a916f1bd789de366b639d09811501e8c092c85b2cb7d697777f93" "79278310dd6cacf2d2f491063c4ab8b129fee2a498e4c25912ddaa6c3c5b621e" "fe94e2e42ccaa9714dd0f83a5aa1efeef819e22c5774115a9984293af609fce7" "730a87ed3dc2bf318f3ea3626ce21fb054cd3a1471dcd59c81a4071df02cb601" "2cdc13ef8c76a22daa0f46370011f54e79bae00d5736340a5ddfe656a767fddf" "dcdd1471fde79899ae47152d090e3551b889edf4b46f00df36d653adc2bf550d" "d71aabbbd692b54b6263bfe016607f93553ea214bc1435d17de98894a5c3a086" "912cac216b96560654f4f15a3a4d8ba47d9c604cbc3b04801e465fb67a0234f0" "bf387180109d222aee6bb089db48ed38403a1e330c9ec69fe1f52460a8936b66" "9efb2d10bfb38fe7cd4586afb3e644d082cbcdb7435f3d1e8dd9413cbe5e61fc" "76bfa9318742342233d8b0b42e824130b3a50dcc732866ff8e47366aed69de11" "2f1518e906a8b60fac943d02ad415f1d8b3933a5a7f75e307e6e9a26ef5bf570" default)))
+ '(fci-rule-color "#62686E")
+ '(jdee-db-active-breakpoint-face-colors (cons "#1c1f24" "#51afef"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#1c1f24" "#7bc275"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#1c1f24" "#484854"))
+ '(objed-cursor-color "#ff665c")
+ '(org-agenda-files nil)
  '(org-startup-truncated nil)
  '(package-selected-packages
    (quote
-    (yasnippet-snippets elixir-yasnippets ember-yasnippets zotxt org-ref ivy-bibtex use-package ox-pandoc pandoc-mode minimap dashboard all-the-icons projectile page-break-lines olivetti darkroom dracula-theme restart-emacs magit)))
+    (counsel-projectile yasnippet-snippets elixir-yasnippets ember-yasnippets zotxt org-ref ivy-bibtex use-package ox-pandoc pandoc-mode minimap dashboard all-the-icons projectile page-break-lines olivetti darkroom dracula-theme restart-emacs magit)))
+ '(pdf-view-midnight-colors (cons "#bbc2cf" "#242730"))
+ '(rustic-ansi-faces
+   ["#242730" "#ff665c" "#7bc275" "#FCCE7B" "#51afef" "#C57BDB" "#5cEfFF" "#bbc2cf"])
+ '(vc-annotate-background "#242730")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#7bc275")
+    (cons 40 "#a6c677")
+    (cons 60 "#d1ca79")
+    (cons 80 "#FCCE7B")
+    (cons 100 "#f4b96e")
+    (cons 120 "#eda461")
+    (cons 140 "#e69055")
+    (cons 160 "#db8981")
+    (cons 180 "#d082ae")
+    (cons 200 "#C57BDB")
+    (cons 220 "#d874b0")
+    (cons 240 "#eb6d86")
+    (cons 260 "#ff665c")
+    (cons 280 "#d15e59")
+    (cons 300 "#a35758")
+    (cons 320 "#754f56")
+    (cons 340 "#62686E")
+    (cons 360 "#62686E")))
+ '(vc-annotate-very-old-color nil)
  '(writeroom-fullscreen-effect (quote maximized))
  '(writeroom-maximize-window nil))
 (custom-set-faces
--- emacs/.emacs.d/settings.el
+++ emacs/.emacs.d/settings.el
@@ -20,8 +20,6 @@ There are two things you can do about this warning:
 ;; (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 ;; (load-theme 'dracula t)
 
-;; (transient-mark-mode 1)
-
 (setq inhibit-splash-screen t)
 
 (menu-bar-mode -1)
@@ -30,68 +28,42 @@ There are two things you can do about this warning:
 
 (require 'dashboard)
 (dashboard-setup-startup-hook)
+(setq dashboard-center-content t)
+
+
+;;   (defun dashboard-insert-custom (list-size)
+;;     (insert "Custom text"))
+;;   (add-to-list 'dashboard-item-generators  '(custom . dashboard-insert-custom))
+;;   (add-to-list 'dashboard-items '(custom) t)
+;;
+;;
+;;
+;;   (setq dashboard-items '((recents  . 0)
+;;			   (bookmarks . 0)
+;;			   (projects . 0)
+;;			   (agenda . 0)
+;;			   (registers . 0)))
 
 (use-package doom-themes
-:ensure t  
-:config
+  :ensure t  
+  :config
   ;; Global settings (defaults)
- ;; (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-   ;;     doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  ;; (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+  ;;     doom-themes-enable-italic t) ; if nil, italics is universally disabled
   (load-theme 'doom-vibrant t)
   (doom-themes-org-config))
 
-  ;; Enable flashing mode-line on errors
- ;; (doom-themes-visual-bell-config)
+;; Enable flashing mode-line on errors
+;; (doom-themes-visual-bell-config)
 
-  ;; Enable custom neotree theme (all-the-icons must be installed!)
-  ;;(doom-themes-neotree-config)
-  ;; or for treemacs users
- ;; (setq doom-themes-treemacs-theme "doom-colors") ; use the colorful treemacs theme
- ;; (doom-themes-treemacs-config)
+;; Enable custom neotree theme (all-the-icons must be installed!)
+;;(doom-themes-neotree-config)
+;; or for treemacs users
+;; (setq doom-themes-treemacs-theme "doom-colors") ; use the colorful treemacs theme
+;; (doom-themes-treemacs-config)
 
-  ;; Corrects (and improves) org-mode's native fontification.
-  ;;(doom-themes-org-config))
-
-(defun dcaps-to-scaps ()
-  "Convert word in DOuble CApitals to Single Capitals."
-  (interactive)
-  (and (= ?w (char-syntax (char-before)))
-       (save-excursion
-         (and (if (called-interactively-p)
-                  (skip-syntax-backward "w")
-                (= -3 (skip-syntax-backward "w")))
-	      (let (case-fold-search)
-                (looking-at "\\b[[:upper:]]\\{2\\}[[:lower:]]"))
-	      (capitalize-word 1)))))
-
-(add-hook 'post-self-insert-hook #'dcaps-to-scaps nil 'local)
-
-
-;; Dubcaps mode
-
-(define-minor-mode dubcaps-mode
-  "Toggle `dubcaps-mode'.  Converts words in DOuble CApitals to
-Single Capitals as you type."
-  :init-value nil
-  :lighter (" DC")
-  (if dubcaps-mode
-      (add-hook 'post-self-insert-hook #'dcaps-to-scaps nil 'local)
-    (remove-hook 'post-self-insert-hook #'dcaps-to-scaps 'local)))
-
-(add-hook 'org-mode-hook 'dubcaps-mode)
-(add-hook 'markdown-mode-hook 'dubcaps-mode)
-
-(use-package flyspell-correct
-      :ensure t
-  :after flyspell
-   :bind (:map flyspell-mode-map ("C-'" . flyspell-correct-wrapper)))
-
- (use-package flyspell-correct-ivy
-     :ensure t 
-  :after flyspell-correct)
-
- ;; Hook to org mode
-;; (add-hook 'org-mode-hook 'flyspell-mode)
+;; Corrects (and improves) org-mode's native fontification.
+;;(doom-themes-org-config))
 
 (global-set-key (kbd "<f5>") 'restart-emacs)
 (global-set-key (kbd "<f6>") 'olivetti-mode)
@@ -125,6 +97,54 @@ Single Capitals as you type."
 
 (global-set-key (kbd "C-;") 'delete-backward-char)
 
+(global-set-key (kbd "C-c C-;") 'elisp-comment-out)
+
+;;(global-set-key (kbd "C-x ,") 'org-cycle-agenda-files)
+
+;; (global-set-key (kbd "C-,")  'windmove-left)
+;; (global-set-key (kbd "C-. <right>") 'windmove-right)
+
+(defun dcaps-to-scaps ()
+  "Convert word in DOuble CApitals to Single Capitals."
+  (interactive)
+  (and (= ?w (char-syntax (char-before)))
+       (save-excursion
+	 (and (if (called-interactively-p)
+		  (skip-syntax-backward "w")
+		(= -3 (skip-syntax-backward "w")))
+	      (let (case-fold-search)
+		(looking-at "\\b[[:upper:]]\\{2\\}[[:lower:]]"))
+	      (capitalize-word 1)))))
+
+(add-hook 'post-self-insert-hook #'dcaps-to-scaps nil 'local)
+
+
+;; Dubcaps mode
+
+(define-minor-mode dubcaps-mode
+  "Toggle `dubcaps-mode'.  Converts words in DOuble CApitals to
+Single Capitals as you type."
+  :init-value nil
+  :lighter (" DC")
+  (if dubcaps-mode
+      (add-hook 'post-self-insert-hook #'dcaps-to-scaps nil 'local)
+    (remove-hook 'post-self-insert-hook #'dcaps-to-scaps 'local)))
+
+(add-hook 'org-mode-hook 'dubcaps-mode)
+(add-hook 'markdown-mode-hook 'dubcaps-mode)
+
+(use-package flyspell-correct
+   :ensure t
+   :after flyspell
+   :bind (:map flyspell-mode-map ("C-'" . flyspell-correct-wrapper)))
+
+ (use-package flyspell-correct-ivy
+  :ensure t 
+  :after flyspell-correct)
+
+ ;; Hook to org mode
+;; (add-hook 'org-mode-hook 'flyspell-mode)
+
 (add-hook 'org-mode-hook 'pandoc-mode)
 
 (eval-after-load "org"
@@ -142,122 +162,6 @@ Single Capitals as you type."
 
 
 (setq org-babel-python-command "python3")
-
-(set-register ?s (cons 'file "~/.emacs.d/settings.org"))
-
-(yas-global-mode 1)
-(global-set-key (kbd "C-c 8") 'yas-insert-snippet)
-
-(global-set-key (kbd "<f7>") 'shell)
-
-(use-package elpy
-  :ensure t
-  :init (elpy-enable)
-  :config (setq elpy-rpc-python-command "python3")
-  )
-
-(defun open-file-fast ()
-  "Prompt to open a file from bookmark `bookmark-bmenu-list'.
-This command is similar to `bookmark-jump', but use `ido-mode' interface, and ignore cursor position in bookmark.
-
-URL `http://ergoemacs.org/emacs/emacs_hotkey_open_file_fast.html'
-Version 2019-02-26"
-  (interactive)
-  (require 'bookmark)
-  (bookmark-maybe-load-default-file)
-  (let (($this-bookmark
-         (ido-completing-read "Open bookmark:" (mapcar (lambda ($x) (car $x)) bookmark-alist))))
-    (find-file (bookmark-get-filename $this-bookmark))
-    ;; (bookmark-jump $this-bookmark)
-    ))
-
-(use-package counsel :ensure t
-  :after ivy
-  :bind (("M-x" . 'counsel-M-x)
-	 ("C-x C-f" . 'counsel-find-file)
-	 ("<f1> f" . 'counsel-describe-function)
-	 ("<f1> v" . 'counsel-describe-variable)
-	 ("<f1> o" . 'counsel-describe-symbol)
-	 ("<f1> l" . 'counsel-find-library)
-	 ("<f2> i" . 'counsel-info-lookup-symbol)
-	 ("<f2> u" . 'counsel-unicode-char)
-	 ("C-c g" . 'counsel-git)
-	 ("C-c j" . 'counsel-git-grep)
-	 ("C-c k" . 'counsel-ag)
-	 ("C-S-o" . 'counsel-rhythmbox)
-	 :map minibuffer-local-map ("C-r" . 'counsel-minibuffer-history)))
-
-(use-package ivy :ensure t
-  :init (setq ivy-use-virtual-buffers t
-	      enable-recursive-minibuffers t)
- :demand  :config (ivy-mode 1)
- :bind (("C-c C-r" . ivy-resume)))
-
-(use-package swiper :ensure t
-  :after ivy
-  :bind (("C-s" . swiper)
-	 ("C-r" . swiper)))
-
-(use-package switch-window
- :ensure t
- :bind (("C-x o" . switch-window)
-	 ("C-x 1" . switch-window-then-maximize)
-	 ("C-x 2" . switch-window-then-split-below)
-	 ("C-x 3" . switch-window-then-split-right)
-	 ("C-x 0" . switch-window-then-delete)
-	 ("C-x 4 d" . switch-window-then-dired)
-	 ("C-x 4 f" . switch-window-then-find-file)
-	 ("C-x 4 m" . switch-window-then-compose-mail)
-	 ("C-x 4 r" . switch-window-then-find-file-read-only)
-	 ("C-x 4 C-f" . switch-window-then-find-file)
-	 ("C-x 4 C-o" . switch-window-then-find-file-read-only)
-	 ("C-x 4 C-f" . switch-window-then-find-file)
-	 ("C-x 4 C-o" . switch-window-then-display-buffer)
-	 ("C-x 4 0" . switch-window-then-kill-buffer)))
-
-(defvar org-blocks-hidden nil)
-
-(defun org-toggle-blocks
-()
-  (interactive)
-  (if org-blocks-hidden
-      (org-show-block-all)
-    (org-hide-block-all))
-  (setq-local org-blocks-hidden (not org-blocks-hidden)))
-
-(add-hook 'org-mode-hook 'org-toggle-blocks)
-
-(define-key org-mode-map (kbd "C-c t") 'org-toggle-blocks)
-
-(setq org-src-tab-acts-natively t)
-
-
-
-(use-package pdf-tools
-  :ensure t
-  :config (pdf-tools-install))
-
-(setq org-agenda-files (apply 'append
-			      (mapcar
-			       (lambda (directory)
-				 (directory-files-recursively
-				  directory org-agenda-file-regexp))
-			       '("~/work/"))))
-
-(define-key global-map "\C-ca" 'org-agenda)
-(setq org-log-done t)
-
-(use-package org-super-agenda
-  :ensure t
-  :config
- (setq org-super-agenda-groups '(
-			   (:name "Waiting"
-			       :tag "shop"))))
-
-(use-package org-journal
-  :ensure t
-  :config
-  (setq org-journal-dir "~/work/journal/"))
 
 ;;  ;;(setq org-ref-completion-library 'org-ref-ivy-cite)
 ;;  (require 'org-ref)
@@ -313,3 +217,255 @@ Version 2019-02-26"
    (assoc-default "title"
                   (bibtex-completion-get-entry
                    (org-ref-get-bibtex-key-under-cursor)))))
+
+(set-register ?s (cons 'file "~/.emacs.d/settings.org"))
+
+(yas-global-mode 1)
+(global-set-key (kbd "C-c 8") 'yas-insert-snippet)
+
+(global-set-key (kbd "<f7>") 'shell)
+
+(use-package elpy
+  :ensure t
+  :init (elpy-enable)
+  :config (setq elpy-rpc-python-command "python3")
+  )
+
+(defun open-file-fast ()
+  "Prompt to open a file from bookmark `bookmark-bmenu-list'.
+This command is similar to `bookmark-jump', but use `ido-mode' interface, and ignore cursor position in bookmark.
+
+URL `http://ergoemacs.org/emacs/emacs_hotkey_open_file_fast.html'
+Version 2019-02-26"
+  (interactive)
+  (require 'bookmark)
+  (bookmark-maybe-load-default-file)
+  (let (($this-bookmark
+         (ido-completing-read "Open bookmark:" (mapcar (lambda ($x) (car $x)) bookmark-alist))))
+    (find-file (bookmark-get-filename $this-bookmark))
+    ;; (bookmark-jump $this-bookmark)
+    ))
+
+(use-package counsel :ensure t
+  :after ivy
+  :bind (("M-x" . 'counsel-M-x)
+	 ("C-x C-f" . 'counsel-find-file)
+	 ("<f1> f" . 'counsel-describe-function)
+	 ("<f1> v" . 'counsel-describe-variable)
+	 ("<f1> o" . 'counsel-describe-symbol)
+	 ("<f1> l" . 'counsel-find-library)
+	 ("<f2> i" . 'counsel-info-lookup-symbol)
+	 ("<f2> u" . 'counsel-unicode-char)
+	 ("C-c g" . 'counsel-git)
+	 ("C-c j" . 'counsel-git-grep)
+	 ("C-c k" . 'counsel-ag)
+	 ("C-S-o" . 'counsel-rhythmbox)
+	 :map minibuffer-local-map ("C-r" . 'counsel-minibuffer-history)))
+
+(use-package ivy :ensure t
+  :init (setq ivy-use-virtual-buffers t
+  enable-recursive-minibuffers t)
+ :demand  :config (ivy-mode 1)
+ :bind (("C-c C-r" . ivy-resume)))
+
+(use-package swiper :ensure t
+  :after ivy
+  :bind (("C-s" . swiper)
+	 ("C-r" . swiper)))
+
+;; (use-package switch-window
+   ;; :ensure t
+   ;; :bind (("C-x o" . switch-window)
+;;	    ("C-x 1" . switch-window-then-maximize)
+;;	    ("C-x 2" . switch-window-then-split-below)
+;;	    ("C-x 3" . switch-window-then-split-right)
+;;	    ("C-x 0" . switch-window-then-delete)
+;;	    ("C-x 4 d" . switch-window-then-dired)
+;;	    ("C-x 4 f" . switch-window-then-find-file)
+;;	    ("C-x 4 m" . switch-window-then-compose-mail)
+;;	    ("C-x 4 r" . switch-window-then-find-file-read-only)
+;;	    ("C-x 4 C-f" . switch-window-then-find-file)
+;;	    ("C-x 4 C-o" . switch-window-then-find-file-read-only)
+;;	    ("C-x 4 C-f" . switch-window-then-find-file)
+;;	    ("C-x 4 C-o" . switch-window-then-display-buffer)
+;;	    ("C-x 4 0" . switch-window-then-kill-buffer)))
+
+(defvar org-blocks-hidden nil)
+
+(defun org-toggle-blocks
+()
+  (interactive)
+  (if org-blocks-hidden
+      (org-show-block-all)
+    (org-hide-block-all))
+  (setq-local org-blocks-hidden (not org-blocks-hidden)))
+
+(add-hook 'org-mode-hook 'org-toggle-blocks)
+
+(define-key org-mode-map (kbd "C-c t") 'org-toggle-blocks)
+
+(setq org-src-tab-acts-natively t)
+
+
+
+(use-package pdf-tools
+  :ensure t
+  :config (pdf-tools-install))
+
+(setq org-agenda-files (apply 'append
+			     (mapcar
+			      (lambda (directory)
+				(directory-files-recursively
+				 directory org-agenda-file-regexp))
+			      '("~/work/agenda/"))))
+ (define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
+
+(use-package org-super-agenda
+  :ensure t
+  :config
+ (setq org-super-agenda-groups '(
+			   (:name "Waiting"
+			       :tag "shop"))))
+
+(use-package org-journal
+  :ensure t
+  :config
+  (setq org-journal-dir "~/work/journal/"))
+
+(defun quick-switch-window ()
+   (interactive)
+  (switch-window))
+(global-set-key (kbd "C-?") 'quick-switch-window)
+
+
+(setq switch-window-shortcut-style 'qwerty)
+
+(use-package centaur-tabs
+  :ensure t
+  :demand
+  :config
+  (centaur-tabs-mode t)
+  :bind
+  ("M-n" . centaur-tabs-backward)
+  ("M-p" . centaur-tabs-forward))
+
+
+(setq centaur-tabs-set-icons t)
+(setq centaur-tabs-plain-icons t)
+
+(use-package all-the-icons)
+
+(use-package treemacs
+  :ensure t
+  :defer t
+  :init
+  (with-eval-after-load 'winum
+    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
+  :config
+  (progn
+    (setq treemacs-collapse-dirs                 (if treemacs-python-executable 3 0)
+          treemacs-deferred-git-apply-delay      0.5
+          treemacs-directory-name-transformer    #'identity
+          treemacs-display-in-side-window        t
+          treemacs-eldoc-display                 t
+          treemacs-file-event-delay              5000
+          treemacs-file-extension-regex          treemacs-last-period-regex-value
+          treemacs-file-follow-delay             0.2
+          treemacs-file-name-transformer         #'identity
+          treemacs-follow-after-init             t
+          treemacs-git-command-pipe              ""
+          treemacs-goto-tag-strategy             'refetch-index
+          treemacs-indentation                   2
+          treemacs-indentation-string            " "
+          treemacs-is-never-other-window         t
+          treemacs-max-git-entries               5000
+          treemacs-missing-project-action        'ask
+          treemacs-move-forward-on-expand        nil
+          treemacs-no-png-images                 nil
+          treemacs-no-delete-other-windows       t
+          treemacs-project-follow-cleanup        nil
+          treemacs-persist-file                  (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
+          treemacs-position                      'left
+          treemacs-recenter-distance             0.1
+          treemacs-recenter-after-file-follow    nil
+          treemacs-recenter-after-tag-follow     nil
+          treemacs-recenter-after-project-jump   'always
+          treemacs-recenter-after-project-expand 'on-distance
+          treemacs-show-cursor                   nil
+          treemacs-show-hidden-files             t
+          treemacs-silent-filewatch              nil
+          treemacs-silent-refresh                nil
+          treemacs-sorting                       'alphabetic-asc
+          treemacs-space-between-root-nodes      t
+          treemacs-tag-follow-cleanup            t
+          treemacs-tag-follow-delay              1.5
+          treemacs-user-mode-line-format         nil
+          treemacs-user-header-line-format       nil
+          treemacs-width                         35)
+
+    ;; The default width and height of the icons is 22 pixels. If you are
+    ;; using a Hi-DPI display, uncomment this to double the icon size.
+    ;;(treemacs-resize-icons 44)
+
+    (treemacs-follow-mode t)
+    (treemacs-filewatch-mode t)
+    (treemacs-fringe-indicator-mode t)
+    (pcase (cons (not (null (executable-find "git")))
+                 (not (null treemacs-python-executable)))
+      (`(t . t)
+       (treemacs-git-mode 'deferred))
+      (`(t . _)
+       (treemacs-git-mode 'simple))))
+  :bind
+  (:map global-map
+        ("M-0"       . treemacs-select-window)
+        ("C-x t 1"   . treemacs-delete-other-windows)
+        ("C-x t t"   . treemacs)
+        ("C-x t B"   . treemacs-bookmark)
+        ("C-x t C-t" . treemacs-find-file)
+        ("C-x t M-t" . treemacs-find-tag)))
+
+(use-package treemacs-evil
+  :after treemacs evil
+  :ensure t)
+
+(use-package treemacs-projectile
+  :after treemacs projectile
+  :ensure t)
+
+(use-package treemacs-icons-dired
+  :after treemacs dired
+  :ensure t
+  :config (treemacs-icons-dired-mode))
+
+(use-package treemacs-magit
+  :after treemacs magit
+  :ensure t)
+
+(use-package treemacs-persp ;;treemacs-persective if you use perspective.el vs. persp-mode
+  :after treemacs persp-mode ;;or perspective vs. persp-mode
+  :ensure t
+  :config (treemacs-set-scope-type 'Perspectives))
+
+(use-package projectile
+    :ensure t
+    :config
+    (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+    (projectile-mode +1)
+    (setq projectile-completion-system 'ivy)
+)
+
+(counsel-projectile-mode t)
+
+;;(windmove-default-keybindings 'meta)
+
+(when (fboundp 'windmove-default-keybindings)
+  (windmove-default-keybindings))
+
+(global-set-key (kbd "M-p") 'switch-window)
+
+(use-package ace-window
+    :ensure t)
+
+(global-set-key (kbd "M-o") 'ace-window)
